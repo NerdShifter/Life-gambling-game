409b4bf48496645f54878b582b175f213d062ee8
NerdShifter <amy0wnself@gmail.com>
2026-02-10 20:44:33 +0100

    Update 10/2/2026
    Just added a few things. Gameplay isnt affected YET

--- GamblingGame.py
+++ GamblingGame.py
@@ -1,52 +1,78 @@
 import random
 
-Name = input("What is your name, player? ")
-Score = 0
+name = input("What is your name, player? ")
+score = 0
+score_multiplier = 1
+Cash = 0
+
 
 class Player:
     def __init__(self, name):
         self.name = name
-        self.Health = 100
-        self.MaxHealth = 100
-        self.MinHealth = 0
+        self.health = 100
+        self.max_health = 100
+        self.min_health = 0
 
     def take_dmg(self, amount):
-        self.Health -= amount
-        if self.Health < self.MinHealth:
-            self.Health = self.MinHealth
-        print("You took", amount, "damage! Current health:", self.Health)
-        if self.Health == self.MinHealth:
+        self.health -= amount
+        if self.health < self.min_health:
+            self.health = self.min_health
+
+        print("You took", amount, "damage! Current health:", self.health)
+
+        if self.health == self.min_health:
             print("Game ended.")
 
-    def regen_Health(self, amount):
-        self.Health += amount
-        if self.Health > self.MaxHealth:
-            self.Health = self.MaxHealth
-        print("You healed", amount, "health! Current health:", self.Health)
+    def regen_health(self, amount):
+        self.health += amount
+        if self.health > self.max_health:
+            self.health = self.max_health
 
-Pl = Player(Name)
-print("Hello,", Pl.name)
-print("You have currently", Pl.Health, "life")
+        print("You healed", amount, "health! Current health:", self.health)
 
-while Pl.Health > Pl.MinHealth:
-    print("Current Score:",Score)
-    print("-"*32)
-    question = input("Would you like to play? (Yes/No) ").lower()
 
-    if question == "yes":
-        Dmg_or_Health = random.randint(1, 50)
-        Chances = random.randint(1, 2)
+pl = Player(name)
+print("Hello,", pl.name)
+print("You have currently", pl.health, "life")
+
+while pl.health > pl.min_health:
+    print("Current Score:", int(score))
+    print("-" * 32)
+    print("What do you want to do?")
+    print("> 1 - Play")
+    print("> 2 - Enter the Shop")
+    print("> 3 - Travel")
+    print("> 4 - Get Player Stats")
+    print("> 5 - End the game")
+
+    question = input("> ").strip()
+
+    if question == "1":
+        dmg_or_health = random.randint(1, 50)
+        chances = random.randint(1, 2)
         print("Deciding your fate...")
-        Score += 10
+        score += 10 * score_multiplier
+        score_multiplier *= 1.1
 
-        if Chances == 1:
-            Pl.take_dmg(Dmg_or_Health)
+        if chances == 1:
+            pl.take_dmg(dmg_or_health)
         else:
-            Pl.regen_Health(Dmg_or_Health)
+            pl.regen_health(dmg_or_health)
 
-    elif question == "no":
+    elif question == "2":
+        print("Shop is not implemented yet.")
+
+    elif question == "3":
+        print("Travelling isn't implemented yet' .")
+        
+    elif question == "4":
+    	print("Current Health:", pl.health)
+    	print("MaxHealth:", pl.max_health)
+
+    elif question == "5":
         break
-    else:
-        print("Please answer Yes or No.")
 
-print("Your score is", Score)
+    else:
+        print("Enter a number")
+
+print("Your score is", int(score))
