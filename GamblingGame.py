f0b1ad61ec8a9c2641c6cb4bafc723439128ace8
NerdShifter <amy0wnself@gmail.com>
2026-02-11 12:08:27 +0100

    Update 11/2/2026
    Made some changes, added an enemy

--- GamblingGame.py
+++ GamblingGame.py
@@ -2,13 +2,7 @@ import random
 
 name = input("What is your name, player? ")
 score = 0
-score_multiplier = 1
-Cash = 0
-
-
-def Wait():
-    input("Press enter to continue.")
-
+cash = 0
 
 class Player:
     def __init__(self, name):
@@ -16,72 +10,90 @@ class Player:
         self.health = 100
         self.max_health = 100
         self.min_health = 0
+        self.strength = 5
 
-    def take_dmg(self, amount):
-        self.health -= amount
-        if self.health < self.min_health:
-            self.health = self.min_health
+class Enemy:
+    def __init__(self):
+        self.health = random.randint(15, 30)
+        self.defense = random.randint(0, 3)
+        self.attack = random.randint(3, 8)
 
-        print("You took", amount, "damage! Current health:", self.health)
-
-        if self.health == self.min_health:
-            print("Game ended.")
-
-    def regen_health(self, amount):
-        self.health += amount
-        if self.health > self.max_health:
-            self.health = self.max_health
-
-        print("You healed", amount, "health! Current health:", self.health)
-
+def wait():
+    input("Press Enter to continue...")
 
 pl = Player(name)
+
 print("Hello,", pl.name)
-print("You have currently", pl.health, "life")
 
 while pl.health > pl.min_health:
-    print("Current Score:", int(score))
-    print("-" * 32)
+
+    print("-" * 30)
     print("What do you want to do?")
-    print("> 1 - Play")
-    print("> 2 - Enter the Shop")
-    print("> 3 - Travel")
-    print("> 4 - Get Player Stats")
-    print("> 5 - End the game")
+    print("> 1 - Fight")
+    print("> 2 - Show Stats")
+    print("> 3 - End Game")
 
-    question = input("> ").strip()
+    choice = input("> ")
 
-    if question == "1":
-        dmg_or_health = random.randint(1, 50)
-        chances = random.randint(1, 2)
-        print("Deciding your fate...")
-        score_added = 10 * score_multiplier
-        score += 10 * score_multiplier
-        score_multiplier *= 1.1
-        print("+", score_added, "Points")
+    if choice == "1":
+        enemy = Enemy()
+        print("An enemy appeared!")
+        print("Enemy Health:", enemy.health)
 
-        if chances == 1:
-            pl.take_dmg(dmg_or_health)
-        else:
-            pl.regen_health(dmg_or_health)
+        while enemy.health > 0 and pl.health > 0:
 
-        Wait()
+            print("-" * 30)
+            print("Your Health:", pl.health)
+            print("Enemy Health:", enemy.health)
+            print("> 1 - Attack")
+            print("> 2 - Run")
 
-    elif question == "2":
-        print("Shop is not implemented yet.")
+            action = input("> ")
 
-    elif question == "3":
-        print("Travelling isn't implemented yet.")
+            if action == "1":
 
-    elif question == "4":
-        print("Current Health:", pl.health)
-        print("MaxHealth:", pl.max_health)
-        Wait()
+                damage = pl.strength - enemy.defense
 
-    elif question == "5":
+                if damage < 0:
+                    damage = 0
+
+                enemy.health -= damage
+                print("You dealt", damage, "damage!")
+
+                if enemy.health > 0:
+                    pl.health -= enemy.attack
+                    print("Enemy hit you for", enemy.attack, "damage!")
+
+            elif action == "2":
+                print("You ran away!")
+                break
+
+            else:
+                print("Invalid option")
+
+        if enemy.health <= 0:
+            print("You defeated the enemy!")
+            score += 10
+            cash += 5
+
+        if pl.health <= 0:
+            print("You died...")
+
+        wait()
+
+    elif choice == "2":
+        print("Name:", pl.name)
+        print("Health:", pl.health)
+        print("Strength:", pl.strength)
+        print("Score:", score)
+        print("Cash:", cash)
+        wait()
+
+    elif choice == "3":
         break
 
     else:
-        print("Enter a number")
+        print("Enter a valid number.")
 
-print("Your score is", int(score))
+print("Game Over.")
+print("Final Score:", score)
